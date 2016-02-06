cdaa6dac964c884e6c2376cd4896da291d71ba48
Fredrik Strupe <frestr@users.noreply.github.com>
2016-02-06 23:29:01 +0100

    Bot class

--- motherrussia.py
+++ motherrussia.py
@@ -1,5 +1,6 @@
 from sockethandler import SocketHandler
 from datahandler import DataHandler
+from bot import Bot
 
 class MotherRussia:
     '''Mother russia functions as the program object'''
@@ -8,6 +9,7 @@ class MotherRussia:
         pass
 
     def init(self):
+        self.bot = Bot()
         self.data_handler = DataHandler()
 
         self.connector = SocketHandler()
@@ -19,3 +21,9 @@ class MotherRussia:
             raw_data = self.connector.poll_data()
             self.data_handler.parse_data(raw_data)
             self.data_handler.print_raw_json()
+
+            self.bot.update()
+
+            last_command = self.bot.get_command()
+            self.connector.send_data(last_command)
+
