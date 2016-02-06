885e1f81e542402344fda322ac1e10e055bafd7a
Fredrik Strupe <frestr@users.noreply.github.com>
2016-02-06 21:10:44 +0100

    Add simple connector interface

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 #!/usr/bin/env python3
+import socket
+import json
+
+class Connector:
+    def __init__(self):
+        pass
+
+    def connect(self, host = ''):
+        self.sock = socket.socket()
+        if host == '':
+            host = socket.gethostname()
+
+        port = 54321
+        self.sock.connect((host, port))
+        
+    def poll_data(self, timeout = None):
+        try:
+            self.sock.settimeout(timeout)
+            data_block = self.sock.recv(2**14)
+            return data_block.decode()
+
+        except socket.timeout:
+            print('Timed out polling data.')
+
+    def send_data(self, string):
+        self.sock.send(string.encode('ascii'))
 
 def main():
-    print("Winning compo...")
+    connector = Connector()
+    connector.connect()
+    connector.send_data('NAME bob\n')
 
-if __name__ == "__main__": # Test
+    while True:
+        data = json.loads(connector.poll_data())
+        print(data)
+
+if __name__ == "__main__": 
     main()
