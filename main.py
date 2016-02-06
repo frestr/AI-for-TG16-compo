78d8b71a4cfb715a0561425dceae5c9fad1de08e
Caronthir <Caronthir@users.noreply.github.com>
2016-02-06 21:55:39 +0100

    Seperated socketHandler and broke the bot

--- main.py
+++ main.py
@@ -1,44 +1,8 @@
 #!/usr/bin/env python3
-import socket
-import json
-import sys
-import time
-
-class Connector:
-    def __init__(self):
-        self.TIMEOUT = 5.0
-        self.PORT = 54321
-        pass
-
-    def connect(self, host = ''):
-        self.sock = socket.socket()
-        if host == '':
-            host = socket.gethostname()
-
-        t_start = time.time()
-        error_msg = ''
-        while time.time() - t_start <= self.TIMEOUT:
-            try:
-                self.sock.connect((host, self.PORT))
-            except OSError as msg:
-                error_msg = msg
-        print('Could not open socket: ', error_msg)
-        sys.exit(1)
-        
-    def poll_data(self, timeout = None):
-        try:
-            self.sock.settimeout(timeout)
-            data_block = self.sock.recv(2**14)
-            return data_block.decode()
-
-        except socket.timeout:
-            print('Timed out polling data.')
-
-    def send_data(self, string):
-        self.sock.send(string.encode('ascii'))
+from socketHandler import socketHandler
 
 def main():
-    connector = Connector()
+    connector = socketHandler()
     connector.connect()
     connector.send_data('NAME bob\n')
 
