1f5ff34e2d5b7fb11818623e7334e441052907da
Fredrik Strupe <frestr@users.noreply.github.com>
2016-02-08 20:53:19 +0100

    Add awesome intro

--- main.py
+++ main.py
@@ -2,12 +2,25 @@
 from motherrussia import MotherRussia
 import argparse
 
+def play_intro():
+    string = '''
+    Ladies and gentlemen, this is ... 
+    ________       __________         
+    ___  __ \___  ___  /___(_)______ 
+    __  /_/ /  / / /  __/_  /__  __ \\
+    _  ____// /_/ // /_ _  / _  / / /
+    /_/     \__,_/ \__/ /_/  /_/ /_/ 
+    '''
+    print(string)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true')
     parser.add_argument('-t', '--timeout', type=float)
     args = parser.parse_args()
+
+    if not args.debug:
+        play_intro()
 
     with MotherRussia(args.debug, args.timeout) as mother:
         mother.init()
